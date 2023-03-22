a8b420c3cca446a3ed451f1582dad281cc381570
Jakub <querly000@gmail.com>
2023-03-22 19:38:02 +0100

    2 step

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,55 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-       home: Scaffold(
-      body: Formularz(),
-       ),
-      );
+    return MaterialApp(
+      home: Scaffold(
+        drawer: Drawer(
+           child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('chujek'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+          ],
+        ),
+        ) ,
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.contact_page_sharp), label: "null"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.safety_check_sharp), label: "chuj")
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+        ),
+        appBar: AppBar(
+          centerTitle: true,
+
+          title: Text("chuj"),
+        ),
+        body: Formularz(),
+      ),
+    );
   }
 }
 
@@ -30,69 +74,74 @@ class _FormularzState extends State<Formularz> {
   final eController = TextEditingController();
   final hController = TextEditingController();
   final cController = TextEditingController();
-
-   @override
-  void dispose(){
-     super.dispose();
-     eController.dispose();
-     hController.dispose();
-     cController.dispose();
-  }
+  String valueToDisplayCos = "";
+  String valueToDisplayEmail = "";
+  String valueToDisplayHaslo = "";
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(20),
-      margin: EdgeInsets.all(5),
-      child: Form(
-        key: formKey,
-         child: Padding(
-           padding: const EdgeInsets.all(25),
-           child: Column(
-           children: [
-             TextFormField(
-                decoration: InputDecoration(
+    return Stack(
+      children: [
+        Image.network('https://docs.flutter.dev/assets/images/dash/dash-fainting.gif',fit: BoxFit.fill,),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Container(
+              margin: EdgeInsets.only(bottom: 5),
+              child: TextFormField(
+                decoration: const InputDecoration(
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.all(
                         Radius.circular(20.0),
                       ),
                     ),
                     hintText: 'email'),
-               controller: eController,
+                controller: eController,
+              ),
             ),
-             TextFormField(
-               decoration: InputDecoration(
-                   border: OutlineInputBorder(
-                     borderRadius: BorderRadius.all(
-                       Radius.circular(20.0),
-                     ),
-                   ),
-                   hintText: 'haslo'),
-               controller: hController,
-             ),
-             TextFormField(
-               decoration: InputDecoration(
-
-                   border: OutlineInputBorder(
-                     borderRadius: BorderRadius.all(
-                       Radius.circular(20.0),
-                     ),
-                   ),
-                   hintText: 'cos'),
-               controller: cController,
-               onChanged: (value){
-                 print(value);
-               },
-             ),
-           ElevatedButton(
-               onPressed: (){},
-               child: Text('Submit'))
-           ],
-          ),
-         ),
-      ),
+            Container(
+              margin: EdgeInsets.only(bottom: 10),
+              child: TextFormField(
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(20.0),
+                      ),
+                    ),
+                    hintText: 'haslo'),
+                controller: hController,
+              ),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(20.0),
+                    ),
+                  ),
+                  hintText: 'cos'),
+              controller: cController,
+              onChanged: (value) {
+                print(value);
+              },
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    valueToDisplayCos = cController.text;
+                    valueToDisplayEmail = eController.text;
+                    valueToDisplayHaslo = hController.text;
+                    valueToDisplayEmail += valueToDisplayHaslo;
+                  });
+                },
+                child: const Text('Submit')),
+            Text(valueToDisplayEmail),
+            Text(valueToDisplayHaslo),
+            Text(valueToDisplayCos),
+          ],
+        ),
+      ],
     );
   }
 }
-
-
